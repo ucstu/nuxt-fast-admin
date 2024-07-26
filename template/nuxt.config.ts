b8eed88d033fa24cb89657d67f5f7a3f3deac5d7
张旭东 <2219454275@qq.com>
2024-07-26 15:09:04 +0800

    2.0.4

--- template/nuxt.config.ts
+++ template/nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@ucstu/nuxt-amis",
-    "@ucstu/nuxt-fast-auth",
-    "@ucstu/nuxt-fast-crud",
-    "nuxt-open-fetch",
-  ],
   extends: ["@ucstu/nuxt-admin-layer"],
 });
