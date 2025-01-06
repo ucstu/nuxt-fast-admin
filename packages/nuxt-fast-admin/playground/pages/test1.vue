51f08c158f2e188ced38e1529aaa915a08a2c99b
张旭东 <2219454275@qq.com>
2025-01-06 17:28:50 +0800

    📃 docs(global): 补充文档

---
+++ packages/nuxt-fast-admin/playground/pages/test1.vue
@@ -0,0 +1,30 @@
+<template>测试页面</template>
+
+<script lang="tsx" setup>
+definePageMeta({
+  icon: 'logos:testcafe',
+  title: '测试页面',
+  desc: '测试页面描述',
+  layout: 'default',
+  head: {
+    has: true,
+  },
+  menu: {
+    has: true,
+    order: 1,
+    desc: '测试页面描述(覆盖)',
+    disabled: false,
+    icon: 'logos:testcafe',
+    parent: 'test',
+    show: true,
+    title: '测试页面(覆盖)',
+  },
+  tab: {
+    desc: '测试页面描述(覆盖)',
+    has: true,
+    icon: 'logos:testcafe',
+    show: true,
+    title: '测试页面(覆盖)',
+  }
+});
+</script>
